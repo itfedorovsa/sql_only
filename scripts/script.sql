cf28f6f70594797149a76769ea9117c50d8a0338
Sergey Fedorov <itfedorovsa@gmail.com>
2026-07-07 01:04:27 +0300

    Refactoring

--- scripts/script.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (full_name, email)
-VALUES ('full_name', 'f@mail.ru');
--- scripts/script1.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (full_name, email)
-VALUES ('full_name', 'h@mail.ru'),
-       ('full_name', 'j@mail.ru');
