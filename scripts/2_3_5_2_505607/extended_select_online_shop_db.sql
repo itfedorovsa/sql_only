6cd2247d256813651b4cca5984f7b315414f312f
Sergey Fedorov <itfedorovsa@gmail.com>
2026-07-07 14:54:50 +0300

    2.3.5.2. Фильтры [#505607 #565261] upd

--- scripts/2_3_5_2_505607/extended_select_online_shop_db.sql
+++ scripts/2_3_5_2_505607/extended_select_online_shop_db.sql
@@ -1,13 +1,27 @@
-SELECT id, name, price, is_active FROM products WHERE is_active = TRUE;
+SELECT id, name, price, is_active
+FROM products
+WHERE is_active = TRUE;
 
-SELECT id, name, price FROM products WHERE price BETWEEN 10000 AND 100000;
+SELECT id, name, price
+FROM products
+WHERE price BETWEEN 10000 AND 100000;
 
-SELECT id, user_id, status, created_at FROM orders WHERE status IN ('NEW', 'PAID');
+SELECT id, user_id, status, created_at
+FROM orders
+WHERE status IN ('NEW', 'PAID');
 
-SELECT id, user_id, status, created_at FROM orders WHERE user_id = 1 AND (status <> 'PAID' OR status IS NULL);
+SELECT id, user_id, status, created_at
+FROM orders
+WHERE user_id = 1 AND (status <> 'CANCELLED' OR status IS NULL);
 
-SELECT id, name, email, created_at FROM users WHERE created_at >= '2026-01-01' AND created_at < '2027-01-01';
+SELECT id, name, email, created_at
+FROM users
+WHERE created_at >= '2026-01-01' AND created_at < '2027-01-01';
 
-SELECT id, name, price FROM products WHERE price NOT BETWEEN 20000 AND 80000;
+SELECT id, name, price
+FROM products
+WHERE price NOT BETWEEN 20000 AND 80000;
 
-SELECT id, name, price FROM products WHERE is_active = TRUE AND NOT BETWEEN 3000 AND 150000;
+SELECT id, name, price
+FROM products
+WHERE is_active = TRUE AND NOT BETWEEN 3000 AND 150000;
